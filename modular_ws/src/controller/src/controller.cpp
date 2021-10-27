01830d41de016da8abcee88e73970939035bf01e
Kerim Moral <moralkerim@gmail.com>
2021-10-27 11:09:04 +0300

    Controller topics changed to eth zurich iris.

--- modular_ws/src/controller/src/controller.cpp
+++ modular_ws/src/controller/src/controller.cpp
@@ -24,6 +24,8 @@
 //Sim includes
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include "controller/attitude.h"
+#include "nav_msgs/Odometry.h"
 #include <tf/tf.h>
 #include "gazebo_msgs/ModelStates.h"
 #include "gazebo_msgs/LinkState.h"
@@ -55,8 +57,9 @@
 
 /* USER CODE BEGIN PV */
 //SIM
-gazebo_msgs::ModelStates sensor_data;
+nav_msgs::Odometry sensor_data;
 gazebo_msgs::LinkState motors;
+controller::attitude attitude;
 
 float gyroX, gyroY, gyroZ, gyro_e_x, gyroX_a,gyroX_a_x, accX, accY, accZ;
 float pitch_acc;
@@ -68,7 +71,7 @@ double roll_des, pitch_des, yaw_des;
 double roll_rate, pitch_rate, yaw_rate;
 double roll_rate_des, pitch_rate_des, yaw_rate_des;
 float w1, w2, w3, w4; //Motor hizlari
-const float pwm_trim = 1800;
+float pwm_trim = 1800;
 const float rad2deg = 180/3.14;
 float S11_m, S12_m, S21_m, S22_m;
 float S11_p, S12_p, S21_p, S22_p;
@@ -77,19 +80,24 @@ float sa = 0.001; float sb = 0.001;
 float Q = 1; //0.5 -- onceki deger.
 float e, e_eski; //PID hatalari
 
+//SIM
+unsigned int start;
+
 
 const unsigned int f = 40;
 const float st = 1/(float)f;
 //PD Katsayilari
-float Kp = 1.2;
-float Kd =  1.5*f;
+float Kp = 0.5;
+float Kd =  0.1*f;
 
-float Kp_angle = 0.03;
+float Kp_angle = 0.03*f;
 int timer;
 unsigned short int IC_val1, IC_val2, pwm_input;
 unsigned short int pwm1, pwm2;
 unsigned short int pwm_mid = 1200;
 char buf[32];
+
+ros::Publisher att_pub;
 
 /* USER CODE END PV */
 
@@ -104,7 +112,7 @@ int Sat(int pwm, int max, int min);
 float pwm2ang(unsigned short int pwm);
 void MotorBaslat(void);
 void Kontrolcu(void);
-void sensorCallback(const gazebo_msgs::ModelStates::ConstPtr& sensor_msg);
+void sensorCallback(const nav_msgs::Odometry::ConstPtr& sensor_msg);
 float pwm2mot(unsigned short int pwm, int dir);
 
 /* USER CODE END PFP */
@@ -120,8 +128,9 @@ int main(int argc, char **argv) {
   /* USER CODE BEGIN 1 */
   ros::init(argc, argv, "controller");
   ros::NodeHandle n;
-  ros::Subscriber sub = n.subscribe("gazebo/model_states", 1000, sensorCallback);
+  ros::Subscriber sub = n.subscribe("iris/odometry_sensor1/odometry", 1000, sensorCallback);
   ros::Publisher motor_pub = n.advertise<gazebo_msgs::LinkState>("gazebo/set_link_state", 1000);
+  att_pub   = n.advertise<controller::attitude>("controller/attitude", 1000);
   ros::Rate loop_rate(f);
 
   /* USER CODE END 1 */
@@ -167,11 +176,20 @@ int main(int argc, char **argv) {
 
     /* USER CODE BEGIN 3 */
 
-    roll_des = 30;
-    pitch_des = 0;
+    roll_des = 0;
+    pitch_des = 15;
     roll_rate_des = P_Angle(roll_des,roll);
     pitch_rate_des = P_Angle(pitch_des,pitch);
     yaw_rate_des = P_Angle(yaw_des,yaw);
+
+    attitude.roll_des = roll_des;
+    attitude.pitch_des = pitch_des;
+    attitude.yaw_des = yaw_des;
+
+
+    attitude.roll_rate_des = roll_rate_des;
+    attitude.pitch_rate_des = pitch_rate_des;
+    attitude.yaw_rate_des = yaw_rate_des;
 /*
     ROS_INFO("roll_rate_des: %.2f",roll_rate_des);
     ROS_INFO("pitch_rate_des: %.2f",pitch_rate_des);
@@ -181,20 +199,20 @@ int main(int argc, char **argv) {
     double pd_pitch = PD_Rate(pitch_rate_des,pitch_rate);
     double pd_yaw   = PD_Rate(yaw_rate_des,yaw_rate);
 
-    pd_roll  = Sat(pd_roll,  400, -400);
-    pd_pitch = Sat(pd_pitch, 400, -400);
-    pd_yaw   = Sat(pd_yaw,   400, -400);
+    pd_roll  = Sat(pd_roll,  200, -200);
+    pd_pitch = Sat(pd_pitch, 200, -200);
+    pd_yaw   = Sat(pd_yaw,   200, -200);
 
     ROS_INFO("pd_roll: %.2f",pd_roll);
     ROS_INFO("pd_pitch: %.2f",pd_pitch);
     ROS_INFO("pd_yaw: %.2f",pd_yaw);
 
-    ROS_INFO("st: %.3f",st);
+    //ROS_INFO("st: %.3f",st);
 
-    unsigned int pwm1 = pwm_trim - pd_roll + pd_pitch;// + pd_yaw;
-    unsigned int pwm2 = pwm_trim + pd_roll - pd_pitch;// + pd_yaw;
-    unsigned int pwm3 = pwm_trim + pd_roll + pd_pitch;// - pd_yaw;
-    unsigned int pwm4 = pwm_trim - pd_roll - pd_pitch;// - pd_yaw;
+    unsigned int pwm1 = pwm_trim + pd_pitch - pd_roll ;// + pd_yaw;
+    unsigned int pwm2 = pwm_trim - pd_pitch + pd_roll ;// + pd_yaw;
+    unsigned int pwm3 = pwm_trim + pd_pitch + pd_roll ;// - pd_yaw;
+    unsigned int pwm4 = pwm_trim - pd_pitch - pd_roll ;// - pd_yaw;
 
     //Saturate pwm values
     pwm1 = Sat(pwm1,PWM_UPPER,PWM_LOWER); pwm2 = Sat(pwm2,PWM_UPPER,PWM_LOWER); pwm3 = Sat(pwm3,PWM_UPPER,PWM_LOWER); pwm4 = Sat(pwm4,PWM_UPPER,PWM_LOWER);
@@ -213,33 +231,38 @@ int main(int argc, char **argv) {
     //motor1
     motors.link_name = "iris::iris_demo::iris::rotor_0";
     motors.twist.angular.z = w1;
-    motors.reference_frame = "iris::iris_demo::iris::rotor_0";
-    motor_pub.publish(motors);
+    motors.reference_frame = "world";
+    //motor_pub.publish(motors);
     //motor2
     motors.link_name = "iris::iris_demo::iris::rotor_1";
     motors.twist.angular.z = w2;
-    motors.reference_frame = "iris::iris_demo::iris::rotor_1";
-    motor_pub.publish(motors);
+    motors.reference_frame = "world";
+    //motor_pub.publish(motors);
     //motor3
     motors.link_name = "iris::iris_demo::iris::rotor_2";
-    motors.reference_frame = "iris::iris_demo::iris::rotor_2";
+    motors.reference_frame = "world";
     motors.twist.angular.z = w3;
-    motor_pub.publish(motors);
+    //motor_pub.publish(motors);
     //motor4
     motors.link_name = "iris::iris_demo::iris::rotor_3";
-    motors.reference_frame = "iris::iris_demo::iris::rotor_3";
+    motors.reference_frame = "world";
     motors.twist.angular.z = w4;
-    motor_pub.publish(motors);
-
+    //motor_pub.publish(motors);
+    if(start > 100) {
+      pwm_trim = 1700;
+    }
+    
     loop_rate.sleep();
     ros::spinOnce();
+    att_pub.publish(attitude);
+    start++;
 
   }
       return 0;
   /* USER CODE END 3 */
 }
 
-void sensorCallback(const gazebo_msgs::ModelStates::ConstPtr& sensor_msg)
+void sensorCallback(const nav_msgs::Odometry::ConstPtr& sensor_msg)
 {
   sensor_data.pose = sensor_msg->pose;
   sensor_data.twist = sensor_msg->twist;
@@ -273,32 +296,45 @@ void Kalman_Filtresi(void) {
 
     //--SIMULASYON--
     //==============
-    tf::Quaternion q(sensor_data.pose[1].orientation.x,
-                     sensor_data.pose[1].orientation.y,
-                     sensor_data.pose[1].orientation.z,
-                     sensor_data.pose[1].orientation.w); 
+    tf::Quaternion q(sensor_data.pose.pose.orientation.x,
+                     sensor_data.pose.pose.orientation.y,
+                     sensor_data.pose.pose.orientation.z,
+                     sensor_data.pose.pose.orientation.w); 
     tf::Matrix3x3 m(q);
     m.getRPY(roll, pitch, yaw);
     roll  =   rad2deg*roll;
     pitch =   -1*rad2deg*pitch;
     yaw   =   rad2deg*yaw;
     
+    attitude.roll = roll;
+    attitude.pitch = pitch;
+    attitude.yaw = yaw;
+
+    /*
     ROS_INFO("roll: %.2f",roll);
     ROS_INFO("pitch: %.2f",pitch);
     ROS_INFO("yaw: %.2f",yaw);
-    
+    */
 
-    roll_rate = sensor_data.twist[1].angular.x;
-    pitch_rate = sensor_data.twist[1].angular.y;
-    yaw_rate = sensor_data.twist[1].angular.z;
+    roll_rate = sensor_data.twist.twist.angular.x;
+    pitch_rate = sensor_data.twist.twist.angular.y;
+    yaw_rate = sensor_data.twist.twist.angular.z;
 
-    roll_rate  = rad2deg*roll_rate;
-    pitch_rate = rad2deg*pitch_rate;
+    roll_rate  = -1*rad2deg*roll_rate;
+    pitch_rate = -1*rad2deg*pitch_rate;
     yaw_rate   = rad2deg*yaw_rate;
 
+    attitude.roll_rate  = roll_rate;
+    attitude.pitch_rate = pitch_rate;
+    attitude.yaw_rate   = yaw_rate;
+
+    
+
+   /*
     ROS_INFO("roll_rate: %.2f",roll_rate);
     ROS_INFO("pitch_rate: %.2f",pitch_rate);
     ROS_INFO("yaw_rate: %.2f",yaw_rate);
+    */
 
     //==============
 }
@@ -326,6 +362,9 @@ double PD_Rate(double alpha_dot_des, double alpha_dot) {
   ROS_INFO("alpha_dot: %.2f",alpha_dot);
   ROS_INFO("Rate error: %.2f",e);
 	de = e - e_eski;
+  ROS_INFO("de error:     %.2f",de);
+  ROS_INFO("e error:      %.2f",e);
+  ROS_INFO("e_eski error: %.2f",e_eski);
 	P = Kp*e; D = Kd*de;
 	pd = P + D;
     return pd;
@@ -363,7 +402,7 @@ float pwm2mot(unsigned short int pwm, int dir) {
 	float in_min  = 1000;
 	float in_max  = 2000;
 	float out_min = 0;
-	float out_max  = 1300;
+	float out_max  = 1100;
 
 	return (float)(dir) * ((float)pwm - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
